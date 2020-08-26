76b3c7993b2d2b679fa8f145d70ecae472b34313
felipe machado <lipizinho-isantos@hotmail.com>
2020-08-26 09:27:17 -0300

    adicionado comentarios de código

--- Localiza_xml_email/program.cs
+++ Localiza_xml_email/program.cs
@@ -1,7 +1,14 @@
-﻿using Limilabs.Client.IMAP;
+﻿//Criador:Felipe Machado Ignacio dos santos
+//ultima atualizaçao 25/08/2020
+//Biblioteca usada no nuget: mail.dll
+
+/*este projeto tem como principal objetivo receber anexos de e-mails não lidos
+recebe os anexos na pasta temp, e depois guarda-os em um servidor na rede.
+logo após avisa o usuario que existe um xml novo no servidor*/
+
+using Limilabs.Client.IMAP;
 using Limilabs.Mail;
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Runtime.InteropServices;
@@ -38,13 +45,11 @@ namespace Localiza_xml_email
             Imap imap = new Imap();
             imap.Connect(Imap_Config);
             imap.UseBestLogin(login_Config, Pass_Config);
-
             imap.SelectInbox();
             List<long> uidList = imap.Search(Flag.Unseen);// recebe a lista de e-mails não lidos
             foreach (long uid in uidList)
             {
                 IMail email = new MailBuilder().CreateFromEml(imap.GetMessageByUID(uid));//variavel recebe o e-mail não lido com todos os atributos
-
                 Classe_email.Quantidade = email.Attachments.Count;//recebe a quantidade de anexos do e-mail
                 if (Classe_email.Quantidade > 0)
                 {
@@ -52,10 +57,8 @@ namespace Localiza_xml_email
                     while (i < Classe_email.Quantidade)//enquanto o contador i for menor que a quantidade de anexos ele executa o que tem dentro do while
                     {
                         Classe_email.Nome = email.Attachments[i].FileName;//recebe o nome do arquivo
-
                         Classe_email.Caminho_Temp = Path.GetTempPath() + Classe_email.Nome; //path da pasta temp do windows + nome do arquivo
                         Classe_email.Caminho_Servidor = @"\\servidor\base\xml_compras\" + DateTime.Now.ToString("MM_yyyy"); // caminho no servidor local, onde serão salvos o xml
-
                         email.Attachments[i].Save(Classe_email.Caminho_Temp);//salva o anexo na pasta temp
                         FileInfo file = new FileInfo(Classe_email.Caminho_Temp);// file info do arquivo temporario
 
@@ -71,7 +74,6 @@ namespace Localiza_xml_email
                                         {
                                             switch (reader.Name.ToString())
                                             {
-
                                                 case "chNFe":
                                                     Classe_email.Chave = reader.ReadString();
                                                     break;
@@ -85,7 +87,6 @@ namespace Localiza_xml_email
                             {
                                 MessageBox((IntPtr)0, "" + e.ToString() + "", "Erro", 0);
                             }
-
                             if (Directory.Exists(Classe_email.Caminho_Servidor))
                                 Copia_arquivo(Classe_email.Caminho_Temp, Classe_email.Caminho_Servidor + @"\" + Classe_email.Chave + ".xml");//metodo que copia mensagem para o servidor e mostra mensagem
                             else
